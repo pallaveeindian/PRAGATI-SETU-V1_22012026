d9303b3c271ea395444de15a4b13a5686382883b
aayushmohan2099 <ayushmohan193@gmail.com>
2026-06-11 11:06:48 +0530

    Updated App routes as per SIRD MEET

--- src/App.jsx
+++ src/App.jsx
@@ -251,14 +251,14 @@ export default function App() {
               element={<TpTrainingRequestClosure />}
             />
             {/* AFTER UIDAI API */}
-            {/* <Route
-              path="/tms/tp/batches/create/:id"
-              element={<TpCreateBatch />}
-            /> */}
             <Route
               path="/tms/tp/batches/create/:id"
-              element={<SiteDevErrorPage />}
+              element={<TpCreateBatch />}
             />
+            {/* <Route
+              path="/tms/tp/batches/create/:id"
+              element={<SiteDevErrorPage />}
+            /> */}
             <Route path="/tms/tp/cp-list" element={<TpListCP />} />
             <Route path="/tms/tp/cp/create" element={<TpCreateCP />} />
             <Route path="/tms/tp/cp/edit/:cpId" element={<TpCreateCP />} />
@@ -350,14 +350,14 @@ export default function App() {
             }
           >
             {/* AFTER UIDAI API */}
-            {/* <Route
-              path="/tms/batch-detail/:id"
-              element={<TrainingBatchDetail />}
-            /> */}
             <Route
               path="/tms/batch-detail/:id"
-              element={<SiteDevErrorPage />}
+              element={<TrainingBatchDetail />}
             />
+            {/* <Route
+              path="/tms/batch-detail/:id"
+              element={<SiteDevErrorPage />}
+            /> */}
           </Route>
 
           {/* Catch-all for unknown TMS paths */}
